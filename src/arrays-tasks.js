dc6e6477773de76c17edf8867ada8dbfcc89f3b6
Asel <asel.alimbaeva.kk@gmail.com>
2024-01-03 08:31:55 +0300

    resolved isValueEqualsIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -188,9 +188,10 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([0, 1, 2, 3, 4]) => true
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((value, index) => value === index);
 }
 
 /**
