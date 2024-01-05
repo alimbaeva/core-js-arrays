b637a38902cc065eecb148f5e0e41362a7115c0e
Asel <asel.alimbaeva.kk@gmail.com>
2024-01-05 20:49:21 +0300

    resolved getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -425,8 +425,12 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce(
+    (cur, el) =>
+      [null, undefined, NaN, false, 0, ''].includes(el) ? cur + 1 : cur,
+    0
+  );
 }
 
 /**
