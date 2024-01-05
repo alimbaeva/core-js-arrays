70eecaa51ff881fabaf820de307af334b3492f05
Asel <asel.alimbaeva.kk@gmail.com>
2024-01-05 21:39:31 +0300

    feat: resolved getMaxItems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -511,8 +511,19 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  if (!arr.length || n === 0) return [];
+
+  const max = Math.max(...arr);
+  const maxIndex = arr.indexOf(max);
+
+  return [
+    max,
+    ...getMaxItems(
+      arr.slice(0, maxIndex).concat(arr.slice(maxIndex + 1)),
+      n - 1
+    ),
+  ];
 }
 
 /**
