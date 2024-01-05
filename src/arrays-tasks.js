335896d0312888070db3a6f3f706e3f82b85d433
Asel <asel.alimbaeva.kk@gmail.com>
2024-01-05 20:28:39 +0300

    resolved getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -408,8 +408,9 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  if (indices.length === 1) return arr[indices];
+  return getElementByIndices(arr[indices.splice(0, 1)], indices);
 }
 
 /**
